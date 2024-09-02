eab7d47d5254b6f84603609b0bcb10adcdc21f3c
Kartik deshmukh <kartik@MelloBs-Mac.local>
2024-09-02 18:57:24 +0530

    Changes

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,33 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <img
+      src="https://cdn.global.noobsverse.com/logos/nvai/Noobsverse.png"
+      width={50}
+      alt="Logo"
+    />
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/Avidia-Cohort",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.gg/b5XqR8yJgu",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/mellob1989/docs.noobsverse.new",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "CoffeeCodes API Docs",
   },
-}
+  faviconGlyph: "📚",
+  banner: {
+    key: "Hackspace Launch",
+    text: (
+      <a href="https://hackspace.in" target="_blank">
+        🎉 Hackspace version 1 is launched! <strong>Read more →</strong>
+      </a>
+    ),
+  },
+};
 
-export default config
+export default config;
